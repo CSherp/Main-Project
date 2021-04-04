0f17a57dd9536044fc2032e7b871ab3dfc5dd7b3
DonnaVolk <donnavolk2@gmail.com>
2021-04-04 15:41:34 +0200

    Begin gemaakt aan registratiescherm

--- SushiLushi/modules/Pages/RegisterPage.cs
+++ SushiLushi/modules/Pages/RegisterPage.cs
@@ -7,17 +7,49 @@ namespace SushiLushi {
             page.Update();
 
             var menu = new UISystem.Menu()
-                .Add("registreren!", () => Console.WriteLine("test123"))
-                .Add("ik wil terug naar start!", GoToStart);
+                .Add("Registreren", Register)
+                .Add("Terug naar startpagina", GoToStart);
 
             menu.Display();
 
-            Console.WriteLine("Hallo dingen enzo");
 
         }
 
         private static void GoToStart() {
             StartPage.Display();
+        }
+        private static void Register() {
+            Console.WriteLine("Voer uw mail in:");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("LET OP: hoofdletter gevoelig");
+            Console.ResetColor();
+            var email = Console.ReadLine();
+
+            Console.WriteLine("Voer uw mail nogmaals in");
+            string repeatEmail = Console.ReadLine();
+
+            while(email != repeatEmail){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("De mailadressen komen niet overeen. Probeer nogmaals:");
+            Console.ResetColor();
+            repeatEmail = Console.ReadLine();
+            }
+
+            Console.WriteLine("Voer uw gebruikersnaam in:");
+            var username = Console.ReadLine();
+
+            Console.WriteLine("Voer uw wachtwoord in:");
+            var password = Console.ReadLine();
+            
+            Console.WriteLine("Voer uw wachtwoord nogmaals in:");
+            string repeatPassword = Console.ReadLine();
+
+            while(password != repeatPassword){
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("De wachtwoorden komen niet overeen. Probeer nogmaals:");
+            Console.ResetColor();
+            repeatPassword = Console.ReadLine();
+            }
         }
     }
 }
