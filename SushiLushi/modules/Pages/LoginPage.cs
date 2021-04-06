572be6725038b60d95ff2b420733c9c96401928a
Raymond <raym0nd.ch8n@gmail.com>
2021-04-06 11:22:40 +0200

    Loginpage

--- SushiLushi/modules/Pages/LoginPage.cs
+++ SushiLushi/modules/Pages/LoginPage.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace SushiLushi {
     class LoginPage {
         public static UISystem.Page page = new UISystem.Page("Login pagina");
@@ -5,14 +7,32 @@ namespace SushiLushi {
             page.Update();
 
             var menu = new UISystem.Menu()
-                .Add("Ik wil inloggen", GoToDash)
-                .Add("Terug naar start", StartPage.Display);
+                .Add("Inloggen", Inloggen)
+                .Add("Terug naar startpagina", GoToStart);
+                // .Add("Inloggen als personeel", InloggenPersoneel);
 
             menu.Display();
         }
 
-        private static void GoToDash() {
-            Dashboard.Display();
+        private static void GoToStart() {
+            StartPage.Display();
+        }
+
+        private static void Inloggen() {
+            Console.WriteLine("Gebruikersnaam:");
+            var username = Console.ReadLine();
+            Console.WriteLine("Wachtwoord:");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("LET OP: hoofdletter gevoelig");
+            Console.ResetColor();
+            var password = Console.ReadLine();
+        }
+
+        private static bool AccountCheck(string username, string password) {
+            foreach(Storage.User user in Storage.System.data.users) {
+    
+            }
+            
         }
     }
 }
