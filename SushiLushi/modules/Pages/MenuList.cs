e2664ef7816f289e7824b9fd847cc787259c4c7c
DonnaVolk <donnavolk2@gmail.com>
2021-05-06 22:34:04 +0200

    Dishes added

--- SushiLushi/modules/Pages/MenuList.cs
+++ SushiLushi/modules/Pages/MenuList.cs
@@ -1,7 +1,80 @@
+using System;
+
 namespace SushiLushi {
-    class MenuList {
+    class MenuList{
+
+        public static void showInfoFish(){
+            page.Update();
+            string[][] Fish = {
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)"}, 
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)", "Avocado", "Bosui", "Sesamzaad", "Mayonaise", "Sriracha"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Sesamzaad","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Zalmfilet", "Sesamzaad", "Bosui"},
+                new string[] {"Tonijnfilet", "Sesamzaad", "Bosui"}
+                };
+                Console.WriteLine("");
+                for (int j = 0; j < Fish[0].Length; j++){
+                    Console.WriteLine($"- {Fish[0][j]}");
+                }
+                var menu = new UISystem.Menu()
+                .Add("Terug naar visgerechten", fish);
+
+            menu.Display();
+            }
+            public static void showInfoFlesh(){
+                page.Update();
+            string[][] Flesh = {
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)"}, 
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)", "Avocado", "Bosui", "Sesamzaad", "Mayonaise", "Sriracha"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Sesamzaad","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Zalmfilet", "Sesamzaad", "Bosui"},
+                new string[] {"Tonijnfilet", "Sesamzaad", "Bosui"}
+                };
+                Console.WriteLine("");
+                for (int j = 0; j < Flesh[0].Length; j++){
+                    Console.WriteLine($"- {Flesh[0][j]}");
+                }
+                var menu = new UISystem.Menu()
+                .Add("Terug naar vleesgerechten", flesh);
+
+            menu.Display();
+            }
+            public static void showInfoVegan(){
+                page.Update();
+            string[][] Vegan = {
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)"}, 
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Zalmfilet","Norivel(zeewier)", "Avocado", "Komkommer", "Tobiko", "Mayonaise", "Wasabi"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Tonijnfilet","Norivel(zeewier)", "Avocado", "Bosui", "Sesamzaad", "Mayonaise", "Sriracha"},
+                new string[] {"Sushirijst","Kristalsuiker","Rijstazijn","Zeezout","Ebi tempura","Norivel(zeewier)", "Sesamzaad","Gebakken ui", "Mayonaise", "Wasabi"},
+                new string[] {"Zalmfilet", "Sesamzaad", "Bosui"},
+                new string[] {"Tonijnfilet", "Sesamzaad", "Bosui"}
+                };
+                Console.WriteLine("");
+                for (int j = 0; j < Vegan[0].Length; j++){
+                    Console.WriteLine($"- {Vegan[0][j]}");
+                }
+                var menu = new UISystem.Menu()
+                .Add("Terug naar vegetarische gerechten", vegan);
+
+            menu.Display();
+            }
+
         public static UISystem.Page page = new UISystem.Page("Menu pagina");
-        public static void Display () {
+        public static void Display(){
             page.Update();
 
             var menu = new UISystem.Menu()
@@ -25,35 +98,63 @@ namespace SushiLushi {
             menu.Display();
 
         }
-        private static void fish() {
-            MenuList.page.Update();
+        public static void fish() {
+            page.Update();
+            var menu = new UISystem.Menu()
+                .Add("Maki zalm", showInfoFish)
+                .Add("Maki tonijn", showInfoFish)
+                .Add("Nigiri zalm", showInfoFish)
+                .Add("Nigiri tonijn", showInfoFish)
+                .Add("California roll", showInfoFish)
+                .Add("Crunchy california roll", showInfoFish)
+                .Add("Spicy tonijn roll", showInfoFish)
+                .Add("Ebi tempura roll", showInfoFish)
+                .Add("Zalm sashimi", showInfoFish)
+                .Add("Tonijn sashimi", showInfoFish)
+                .Add("Terug naar gerechten", lijst);
+
+            menu.Display();
+        }
+        public static void flesh() {
+            page.Update();
 
             var menu = new UISystem.Menu()
-                .Add("Maki zalm", showInfo)
-                .Add("Maki tonijn", showInfo)
-                .Add("Nigiri zalm", showInfo)
-                .Add("Nigiri tonijn", showInfo)
-                .Add("California roll", showInfo)
-                .Add("Spicy tonijn roll", showInfo)
-                .Add("Crunchy california roll", showInfo)
-                .Add("Ebi tempura garnaal roll", showInfo)
-                .Add("Zalm sashimi", showInfo)
-                .Add("Tonijn sashimi", showInfo)
-                .Add("Terug", lijst);
+                .Add("Maki zalm", showInfoFlesh)
+                .Add("Maki tonijn", showInfoFlesh)
+                .Add("Nigiri zalm", showInfoFlesh)
+                .Add("Nigiri tonijn", showInfoFlesh)
+                .Add("California roll", showInfoFlesh)
+                .Add("Crunchy california roll", showInfoFlesh)
+                .Add("Spicy tonijn roll", showInfoFlesh)
+                .Add("Ebi tempura roll", showInfoFlesh)
+                .Add("Zalm sashimi", showInfoFlesh)
+                .Add("Tonijn sashimi", showInfoFlesh)
+                .Add("Terug naar gerechten", lijst);
+
+            menu.Display();
         }
-        private static void flesh() {
-            Dashboard.Display();
-        }
-        private static void vegan() {
-            Dashboard.Display();
+        public static void vegan() {
+            page.Update();
+
+            var menu = new UISystem.Menu()
+                .Add("Maki zalm", showInfoVegan)
+                .Add("Maki tonijn", showInfoVegan)
+                .Add("Nigiri zalm", showInfoVegan)
+                .Add("Nigiri tonijn", showInfoVegan)
+                .Add("California roll", showInfoVegan)
+                .Add("Crunchy california roll", showInfoVegan)
+                .Add("Spicy tonijn roll", showInfoVegan)
+                .Add("Ebi tempura roll", showInfoVegan)
+                .Add("Zalm sashimi", showInfoVegan)
+                .Add("Tonijn sashimi", showInfoVegan)
+                .Add("Terug naar gerechten", lijst);
+
+            menu.Display();
+    
         }       
 
-        private static void all() {
+        public static void all() {
             Dashboard.Display();
         } 
-
-        private static void showInfo() {
-            Dashboard.Display();
-        }
     }
 }
