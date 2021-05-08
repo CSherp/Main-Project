3d092d1fb5e97b6aa5bb037059137f4b4022940c
Valeria <valeria.prakopchyk@gmail.com>
2021-05-08 22:50:17 +0200

    eetwensen voor meerdere pers

--- SushiLushi/modules/Pages/ReserveerPage.cs
+++ SushiLushi/modules/Pages/ReserveerPage.cs
@@ -24,9 +24,9 @@ namespace SushiLushi {
             Console.WriteLine("Kies een dag:");
 
             var menu2 = new UISystem.Menu()
-                .Add(today.AddDays(1).ToString("dddd") + " " + today.AddDays(1).Date.ToString("dd-MM"), null)
-                .Add(today.AddDays(2).ToString("dddd") + " " + today.AddDays(2).Date.ToString("dd-MM"), null)
-                .Add(today.AddDays(3).ToString("dddd") + " " + today.AddDays(3).Date.ToString("dd-MM"), null);
+                .Add(today.AddDays(1).ToString("dddd") + " " + today.AddDays(1).Date.ToString("dd-MM"))
+                .Add(today.AddDays(2).ToString("dddd") + " " + today.AddDays(2).Date.ToString("dd-MM"))
+                .Add(today.AddDays(3).ToString("dddd") + " " + today.AddDays(3).Date.ToString("dd-MM"));
 
             menu2.Display();
 
@@ -37,20 +37,49 @@ namespace SushiLushi {
             Console.ResetColor();            
             Console.WriteLine("Kies een tijd: ");
             
-            var menu3 = new UISystem.Menu()
-                .Add("Vanaf 13:00", null)
-                .Add("Vanaf 16:00", null)
-                .Add("Vanaf 19:00", null);
+            string[] Arr = new string[] {"Vanaf 13:00","Vanaf 16:00","Vanaf 19:00"};
+
+            var menu3 = new UISystem.Menu();
+                foreach (string value in Arr) {
+                    menu3.Add(value);
+                }
 
             menu3.Display();
-
+            
             int index_menu3 = menu3.GetSelectedIndex();
             Console.Write("Je hebt gekozen voor: ");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(today.AddDays(index_menu2).ToString("dddd") + " " + today.AddDays(index_menu2).Date.ToString("dd-MM") + " om ..... ");
+            Console.WriteLine(today.AddDays(index_menu2).ToString("dddd") + " " + today.AddDays(index_menu2).Date.ToString("dd-MM") + " " + Arr[index_menu3 - 1]);
             Console.ResetColor();  
             
-            string dieet_wensen = UISystem.Input.ReadString("Heeft u dieet wensen/ alergieën?");
+            int[] Arr_Opties = new int[aantal_mensen];  
+            string[] Arr2 = new string[] {"Geen","Vegetarisch","Veganistisch","Glutenvrij","Notenallergie"};
+            
+            int counter_mensen = 1; 
+            for (int i = 0; i < aantal_mensen; i++) { 
+                Console.Write("Heeft u dieet wensen/ alergieën?");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(" Persoon " + counter_mensen);
+                Console.ResetColor();  
+
+                var menu4 = new UISystem.Menu();
+                    foreach (string value in Arr2) {
+                        menu4.Add(value);
+                    }
+
+                menu4.Display();
+
+                int index_menu4 = menu4.GetSelectedIndex();
+                Arr_Opties[i] = index_menu4; 
+                counter_mensen++; 
+            }
+            
+            Console.WriteLine("Eetwensen personen: ");
+            for (int i = 0; i < aantal_mensen; i++) {
+                int optie_menu = Arr_Opties[i];
+                int index_onnodig = i+1;
+                Console.WriteLine("Persoon " + index_onnodig + ": " + Arr2[optie_menu-1]); 
+            }
         }
         private static void GoToStart() {
             StartPage.Display();        }
