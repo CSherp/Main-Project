4d4db071c4e4da42fce255569792c002747a44e6
Dejan <dejans01@hotmail.com>
2021-06-01 13:14:08 +0200

    fix

--- SushiLushi/modules/Pages/StartPage.cs
+++ SushiLushi/modules/Pages/StartPage.cs
@@ -48,78 +48,7 @@ namespace SushiLushi {
 
             static void PrintBottom(){
 
-                //Array met mogelijke keuzes
-                string[] Choice = {"Meat", "Fish", "Vegan"};
                 
-                //Array met alle maaltijden van "Meat", "Fish" en "Vegan"
-                string[][] MenuLijst = {
-                    new string[] {"Surf n' turf roll", "Carpaccio roll", "Beef roll", "Tournedos", "Usuyaki", "Beef lettuce wrap", "Japanese burger", "Beef tataki"},
-                    new string[] {"Maki zalm", "Maki tonijn", "Nigiri zalm", "Nigiri tonijn", "California roll", "Crunchy california roll", "Spicy tonijn roll", "Ebi tempura roll", "Zalm sashimi", "Tonijn sashimi"},
-                    new string[] {"Nigir tamago", "Maki Komkommer", "Maki avocado", "Crispy mayo roll", "Wakame tamaki", "Edamame"}
-                };
-                string[] show = new string[4];
-                int showAmount = 4;
-                int index;
-                string food;
-
-                //Zorgt voor 4 willekeurige maaltijden en stopt ze in de array "show"
-                for (int i = 0; i < showAmount; i++){
-                    index = Random(Choice.Length);
-                    food = MenuLijst[index][Random(MenuLijst[index].Length)];
-                    bool run = true;
-                    while (run){
-                        food = MenuLijst[index][Random(MenuLijst[index].Length)];
-                        if (show[0] == null && food.Length == 9 && !show.Contains(food)){
-                            show[0] = food;
-                            run = false;
-                        }
-                        else if (show[1] == null && food.Length == 11 && !show.Contains(food)){
-                            show[1] = food;
-                            run = false;
-                        }
-                        else if (show[2] == null && food.Length == 14 && !show.Contains(food)){
-                            show[2] = food;
-                            run = false;
-                        }
-                        else if (show[3] == null && food.Length == 12 && !show.Contains(food)){
-                            show[3] = food;
-                            run = false;
-                        }
-                    }
-                }
-
-                //Zorgt voor 4 willekeurige prijzen voor de maaltijden
-                // string[] price = {"6.75", "7.50", "8.00", "8.50", "9.00", "9.50", "10.00", "12.50", "14.00", "15.00"};
-                // string[] ChosenPrices = new string[4];
-                // for (int i = 0; i < showAmount; i++){
-                //     string randomprice = price[Random(price.Length)];
-                //     bool run = true;
-                //     while (run){
-                //         if (ChosenPrices[0] == null && randomprice.Length == 4 && !ChosenPrices.Contains(randomprice)){
-                //             ChosenPrices[0] = randomprice;
-                //             run = false;
-                //             break;
-                //         }
-                //         else if (ChosenPrices[1] == null && randomprice.Length == 5 && !ChosenPrices.Contains(randomprice)){
-                //             ChosenPrices[1] = randomprice;
-                //             run = false;
-                //             break;
-                //         }
-                //         else if (ChosenPrices[2] == null && randomprice.Length == 4 && !ChosenPrices.Contains(randomprice)){
-                //             ChosenPrices[2] = randomprice;
-                //             run = false;
-                //             break;
-                //         }
-                //         else if (ChosenPrices[3] == null && randomprice.Length == 5 && !ChosenPrices.Contains(randomprice)){
-                //             ChosenPrices[3] = randomprice;
-                //             run = false;
-                //             break;
-                //         }
-                //     }
-                   
-                //}
-
-                //Tuple<string, string, string, string> prices = Tuple.Create(ChosenPrices[0], ChosenPrices[1], ChosenPrices[2], ChosenPrices[3]);
 
 
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -129,8 +58,8 @@ namespace SushiLushi {
                 Console.WriteLine(@".s.                                                                                    .s.");
                 Console.WriteLine(@".s.                               Maaltijden van de dag:                               .s.");
                 Console.WriteLine(@".s.                                                                                    .s.");
-                Console.WriteLine($".s.                 {show[0]}:                   .s.");
-                Console.WriteLine($".s.                 {show[1]}:                  .s.");
+                Console.WriteLine($".s.                 Persoon 12+ dinner :                                               .s.");
+                Console.WriteLine($".s.                                                                                    .s.");
                 Console.WriteLine(@".s.                                                                                    .s.");
                 Console.WriteLine(@"`/::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/`");
                 Console.WriteLine(@".s:------------------------------------------------------------------------------------:s.");
@@ -158,16 +87,64 @@ namespace SushiLushi {
             }
             PrintTop();
             PrintBottom();
-            UISystem.Output.WriteLine(System.ConsoleColor.White, "Welkom bij Sushi Lushi, het beste Sushi Restaurant in de wereld!");
+            UISystem.Output.WriteLine(System.ConsoleColor.White, "");
+            UISystem.Output.WriteLine(System.ConsoleColor.White, "Welkom bij Sushi Lushi, het beste Sushi restaurant in de wereld!");
             UISystem.Output.WriteLine(System.ConsoleColor.White, "Maak uw keuze!");
-            UISystem.Output.WriteLine(System.ConsoleColor.White, "--------------------------");
-            var menu = new UISystem.Menu()
-                .Add("Login", LoginPage.Display)
-                .Add("Registreren", RegisterPage.Display)
-                .Add("Reserveren", ReserveerPage.Display)
-                .Add("Menu", MenuList.Display);
-            
+
+            var menu = new UISystem.Menu();
+
+            // Niet ingelogd
+            if (!Storage.SushiLushiState.isLoggedIn) {
+                menu.Add("Login", LoginPage.Display);
+                menu.Add("Registreren", RegisterPage.Display);
+                
+                menu.Add("Reserveren", ReserveerPage.Display);
+                menu.Add("Menu bekijken", MenuList.Display);
+            }
+
+            // Ingelogd als gebruiker
+            if (Storage.SushiLushiState.isLoggedIn && !Storage.SushiLushiState.isAdmin) {
+                menu.Add("Reserveren", ReserveerPage.Display);
+                menu.Add("Mijn reserveringen", listReservations);
+                menu.Add("Menu bekijken", MenuList.Display);
+            }
+
+            // Ingelogd als admin
+            if (Storage.SushiLushiState.isLoggedIn && Storage.SushiLushiState.isAdmin) {
+                menu.Add("Gebruikers beheren", AdminPageUser.Display);
+                menu.Add("Reserveringen beheren", AdminPageReservations.Display);
+            }
+
+            if (Storage.SushiLushiState.isLoggedIn) {
+                menu.Add("Uitloggen", logout);
+            }
+
+
             menu.Display();
-        }   
+        } 
+        public static void logout() {
+            Storage.SushiLushiState.isLoggedIn = false;
+            Storage.SushiLushiState.isAdmin = false;
+            Storage.SushiLushiState.loggedUser = null;
+            
+            UISystem.Input.ReadString("U bent uitgelogd! (Druk op enter om verder te gaan)");
+            StartPage.Display();
+        }
+
+        public static void listReservations() {
+            UISystem.Output.WriteLine(ConsoleColor.Cyan, "Dit zijn uw reserveringen:");
+
+            foreach (Storage.Reservation reservation in Storage.System.data.reservations) {
+                if (reservation.username == Storage.SushiLushiState.loggedUser.username) {
+                    UISystem.Output.WriteLine(ConsoleColor.Green, "");
+                    UISystem.Output.WriteLine(ConsoleColor.Green, "| Datum: " + reservation.datetime.ToString());
+                    UISystem.Output.WriteLine(ConsoleColor.Green, "| Aantal personen: " + reservation.amountPeople);
+                    UISystem.Output.WriteLine(ConsoleColor.Green, "");
+                }
+            }
+            
+            UISystem.Input.ReadString("(Druk op enter om verder te gaan)");
+            StartPage.Display();
+        }  
     }
 }
